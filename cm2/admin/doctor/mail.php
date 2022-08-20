632fbf945fa6586308c1b80f812ef7ea3f31b95c
Wolvan <wolvan1@gmail.com>
2022-08-20 22:10:49 +0200

    Fix up sendmail test
    Using an invalid email address failed for some reason.
    Instead a catchall email from mailinator.com is used to test, as most
    SMTP servers accept that email.

--- cm2/admin/doctor/mail.php
+++ cm2/admin/doctor/mail.php
@@ -3,7 +3,7 @@
 error_reporting(0);
 header('Content-Type: text/plain');
 
-if(mail('to@example.invalid', 'subject', 'body', 'from@example.invalid')) {
+if(mail('catchall@mailinator.com', 'subject', 'body', 'from@example.invalid')) {
 	echo 'OK PHP appears capable of sending email. Please verify with a test registration.';
 } else {
 	echo 'NG PHP does not appear capable of sending email. Check PHP and SendMail configuration.';
