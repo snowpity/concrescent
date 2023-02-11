cd913cd3421aca197000e34405ccacb5ce65bde5
Wolvan <wolvan1@gmail.com>
2023-02-11 17:58:43 +0100

    Create API that exposed Room Schedule
    This allows building a frontend in the future to display time slots and
    schedules for the convention

---
+++ cm2/api/schedule.php
@@ -0,0 +1,23 @@
+<?php
+require_once dirname(__FILE__) . "/../config/config.php";
+require_once dirname(__FILE__) . "/../lib/database/application.php";
+require_once dirname(__FILE__) . "/../lib/database/forms.php";
+
+$context = isset($_GET["c"]) ? trim($_GET["c"]) : null;
+
+if (!$context) {
+    header("Location: ../", true, 303);
+    exit;
+}
+$db = new cm_db();
+$apdb = new cm_application_db($db, $context);
+$fdb = new cm_forms_db($db, "application-".strtolower($context));
+$assignments = $apdb->list_room_and_table_assignments(null, strtoupper($context));
+$formQuestions = $fdb->list_questions();
+$formAnswers = array();
+foreach ($assignments as &$assignment)
+    foreach ($formQuestions as $question)
+        $assignment[$question["title"]] = $fdb->get_answer($assignment["context-id"], $question["question-id"]);
+$response = ["ok" => true, "assignments" => $assignments];
+header("Content-Type: application/json; charset=utf-8", true, 200);
+echo json_encode($response);
