62e0b2e5f991e9eb7552986ee6a4f7184323e073
Kahas <kahas@gmx.com>
2023-12-16 03:24:30 +0100

    global twig

--- cm2/admin/logout.php
+++ cm2/admin/logout.php
@@ -6,16 +6,16 @@ session_start();
 require_once dirname(__FILE__).'/../lib/database/database.php';
 require_once dirname(__FILE__).'/../lib/database/admin.php';
 require_once dirname(__FILE__).'/../lib/util/res.php';
+global $twig;
 
 $db = new cm_db();
 $adb = new cm_admin_db($db);
 $adb->log_access();
 $adb->log_out();
 
-
 try {
     // Render the Twig template with the variables
-    echo $twig->render('logout.twig', [
+    echo $twig->render('admin/logout.twig', [
 		'shortcutIcon' => theme_file_url('favicon.ico', false),
 		'stylesheet1' => resource_file_url('cm.css', false),
 		'stylesheet2' => theme_file_url('theme.css', false),
--- cm2/lib/util/res.php
+++ cm2/lib/util/res.php
@@ -2,13 +2,12 @@
 
 require_once dirname(__FILE__).'/../../config/config.php';
 require_once dirname(__FILE__).'/util.php';
-require_once '../../vendor/autoload.php';
+require_once __DIR__.'/../../../vendor/autoload.php';
 
 $twig = new \Twig\Environment(
-	new \Twig\Loader\FilesystemLoader(__DIR__.'../../templates'),
+	new \Twig\Loader\FilesystemLoader(__DIR__.'/../../../templates'),
 	['debug' => true]
 );
-
 
 function config_file_path($file) {
 	return realpath(dirname(__FILE__) . '/../../config') . '/' . $file;
