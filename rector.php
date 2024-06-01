743631a21f1f3d3e392f9cb128a2c1c7ea5343d8
Kahas <kahas@gmx.com>
2024-06-01 20:26:14 +0200

    introduce rector

---
+++ rector.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/cm2',
+    ])
+    ->withPhpSets()
+    ->withRules([
+        AddVoidReturnTypeWhereNoReturnRector::class,
+    ]);
