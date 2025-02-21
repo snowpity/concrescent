6a3c8434952bb7c11ce06306ea0952652852fbbd
Mr. Metric <thingies.email@gmail.com>
2025-02-20 19:32:25 -0800

    fix minor inconsistency with other files

--- cm2/admin/doctor/database3.php
+++ cm2/admin/doctor/database3.php
@@ -10,7 +10,7 @@ $phptime = date('Y-m-d H:i:s');
 
 $diff = abs(strtotime($dbtime) - strtotime($phptime));
 if ($diff > 600) {
-	exit('NG MySQL time and PHP time differ by over 10 minutes. Check time zone settings, make sure time zone data is present in MySQL, and run /admin/timecheck.php to verify.');
+	die('NG MySQL time and PHP time differ by over 10 minutes. Check time zone settings, make sure time zone data is present in MySQL, and run /admin/timecheck.php to verify.');
 }
 
 echo 'OK MySQL time and PHP time are synchronized';
