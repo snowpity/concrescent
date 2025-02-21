86d6ecf61b4454358e7b56c34a7b5fcf9895e2f7
Mr. Metric <thingies.email@gmail.com>
2025-02-20 19:19:32 -0800

    fix up the doctor to improve tests and their output

--- cm2/admin/doctor/database4.php
+++ cm2/admin/doctor/database4.php
@@ -8,13 +8,15 @@ $db = new cm_db();
 $charset = $db->connection->character_set_name();
 $charsets = $db->characterset();
 
-if (
-	$charset == 'utf8'
-	&& str_starts_with($charsets['character_set_client'], 'utf8')
-	&& str_starts_with($charsets['character_set_connection'], 'utf8')
-	&& str_starts_with($charsets['character_set_results'], 'utf8')
+// NOTE: character_set_connection === $db->connection->character_set_name()
+// The cm_db constructor sets the connection's encoding to utf8mb4,
+// and that's what all of these should be for full Unicode® support.
+if ($charsets['character_set_client'    ] === 'utf8mb4'
+ && $charsets['character_set_connection'] === 'utf8mb4'
+ && $charsets['character_set_database'  ] === 'utf8mb4'
+ && $charsets['character_set_results'   ] === 'utf8mb4'
 ) {
 	echo 'OK MySQL is using UTF-8.';
 } else {
-	echo 'NG MySQL is not using UTF-8.';
+	echo "NG MySQL is not using UTF-8. Charsets: client = $charsets[character_set_client], connection = $charsets[character_set_connection], database = $charsets[character_set_database], results = $charsets[character_set_results].";
 }
