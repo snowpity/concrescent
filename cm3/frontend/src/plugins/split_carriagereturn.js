400820de9e0fc32745778a1e4db26a4dc2e2f209
Kahas <kahas@gmx.com>
2025-06-19 08:30:06 +0200

    adios cm3

--- cm3/frontend/src/plugins/split_carriagereturn.js
+++
@@ -1,4 +0,0 @@
-export function split_carriagereturn(value) {
-  if (typeof value !== 'string') { return null; }
-  return value.split('\n');
-}
--- cm3/frontend/src/routes/register/checkout.vue
+++
@@ -1,9 +0,0 @@
-<template>
-<pre>
-Status message
-
-Offer to create account?
-
-Kiosk mode instructions and auto-button to start a new one.
-</pre>
-</template>
